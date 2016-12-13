361c6a93eb4fd14cbe3dec1539a879bab8ef9f4c
Kina <CrafterKina@users.noreply.github.com>
2016-12-13 22:45:02 +0900

    Add ObjectHolder Annotation Suppress unused

--- src/main/java/jp/crafterkina/pipes/common/RegistryEntries.java
+++ src/main/java/jp/crafterkina/pipes/common/RegistryEntries.java
@@ -1,6 +1,11 @@
 package jp.crafterkina.pipes.common;
 
-@SuppressWarnings({"AssignmentToNull","NullableProblems"})
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+import static jp.crafterkina.pipes.common.PipesCore.MOD_ID;
+
+@ObjectHolder(MOD_ID)
+@SuppressWarnings({"AssignmentToNull", "NullableProblems", "unused"})
 public class RegistryEntries {
 
 }
