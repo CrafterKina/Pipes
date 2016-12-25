169f661f357fdbefbe94e75814129cd9ee75447b
Kina <CrafterKina@users.noreply.github.com>
2016-12-25 17:40:17 +0900

    Add Pipe Recipes

---
+++ src/main/java/jp/crafterkina/pipes/common/recipe/CoverPipeRecipe.java
@@ -0,0 +1,76 @@
+package jp.crafterkina.pipes.common.recipe;
+
+import jp.crafterkina.pipes.common.RegistryEntries;
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by Kina on 2016/12/25.
+ */
+public class CoverPipeRecipe implements IRecipe{
+    public static final IRecipe CARPET = new ShapedOreRecipe(new ItemStack(RegistryEntries.ITEM.pipe, 1), "CCC", "CPC", "CCC", 'C', new ItemStack(Blocks.CARPET, 1, OreDictionary.WILDCARD_VALUE), 'P', new ItemStack(RegistryEntries.ITEM.pipe, 1, OreDictionary.WILDCARD_VALUE));
+    public static final IRecipe WOOL = new ShapedOreRecipe(new ItemStack(RegistryEntries.ITEM.pipe, 1), " W ", "WPW", " W ", 'W', new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE), 'P', new ItemStack(RegistryEntries.ITEM.pipe, 1, OreDictionary.WILDCARD_VALUE));
+
+    @Override
+    public boolean matches(@Nonnull InventoryCrafting inv, @Nonnull World worldIn){
+        if(CARPET.matches(inv, worldIn)){
+            int damage = inv.getStackInSlot(0).getItemDamage();
+            for(int i = 0; i < inv.getSizeInventory(); i++){
+                ItemStack slot = inv.getStackInSlot(i);
+                if(slot.getItem() == Item.getItemFromBlock(Blocks.CARPET) && damage != slot.getItemDamage())
+                    return false;
+            }
+            return true;
+        }else if(WOOL.matches(inv, worldIn)){
+            int damage = inv.getStackInSlot(1).getItemDamage();
+            for(int i = 0; i < inv.getSizeInventory(); i++){
+                ItemStack slot = inv.getStackInSlot(i);
+                if(slot.getItem() == Item.getItemFromBlock(Blocks.WOOL) && damage != slot.getItemDamage()) return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack getCraftingResult(@Nonnull InventoryCrafting inv){
+        ItemStack result = inv.getStackInRowAndColumn(1, 1).copy();
+        int color = ItemDye.DYE_COLORS[EnumDyeColor.byMetadata(inv.getStackInSlot(1).getItemDamage()).getDyeDamage()];
+        NBTTagCompound compound = result.getTagCompound();
+        if(compound == null) return ItemStack.EMPTY;
+        compound.setBoolean("covered", true);
+        compound.setInteger("color", color);
+        return result;
+    }
+
+    @Override
+    public int getRecipeSize(){
+        return 9;
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack getRecipeOutput(){
+        return new ItemStack(RegistryEntries.ITEM.pipe);
+    }
+
+    @Nonnull
+    @Override
+    public NonNullList<ItemStack> getRemainingItems(@Nonnull InventoryCrafting inv){
+        return ForgeHooks.defaultRecipeGetRemainingItems(inv);
+    }
+}
--- src/main/java/jp/crafterkina/pipes/common/recipe/vanilla/CraftManager.java
+++ src/main/java/jp/crafterkina/pipes/common/recipe/vanilla/CraftManager.java
@@ -1,9 +1,17 @@
 package jp.crafterkina.pipes.common.recipe.vanilla;
 
+import jp.crafterkina.pipes.common.RegistryEntries;
+import jp.crafterkina.pipes.common.pipe.EnumPipeMaterial;
+import jp.crafterkina.pipes.common.recipe.CoverPipeRecipe;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.RecipeSorter;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 import static jp.crafterkina.pipes.common.RegistryEntries.ITEM.merchant_phone;
 
@@ -13,5 +21,38 @@ public enum CraftManager{
     public void register(){
         GameRegistry.addRecipe(new ItemStack(merchant_phone), "IRI", "IGI", "IEI", 'G', Items.EMERALD, 'R', Blocks.REDSTONE_TORCH, 'E', Items.ENDER_PEARL, 'I', Items.IRON_INGOT);
         GameRegistry.addShapelessRecipe(new ItemStack(merchant_phone), merchant_phone);
+        GameRegistry.addRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.WOOD), "SLS", "L L", "SLS", 'S', Items.STICK, 'L', Blocks.LADDER);
+        GameRegistry.addRecipe(new ShapedOreRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.STONE), "SLS", "L L", "SLS", 'S', "cobblestone", 'L', Blocks.LADDER));
+        GameRegistry.addRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.IRON), "SLS", "L L", "SLS", 'S', Items.IRON_INGOT, 'L', Blocks.RAIL);
+        GameRegistry.addRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.DIAMOND), "SLS", "L L", "SLS", 'S', Items.DIAMOND, 'L', Blocks.RAIL);
+        GameRegistry.addRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.GOLD), "SLS", "L L", "SLS", 'S', Items.GOLD_NUGGET, 'L', Blocks.GOLDEN_RAIL);
+        for(EnumDyeColor color : EnumDyeColor.values()){
+            GameRegistry.addRecipe(new ShapedOreRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.WOOD, color), "SLS", "LWL", "SLS", 'S', Items.STICK, 'L', Blocks.LADDER, 'W', new ItemStack(Blocks.WOOL, 1, color.getMetadata())));
+            GameRegistry.addRecipe(new ShapedOreRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.STONE, color), "SLS", "LWL", "SLS", 'S', "cobblestone", 'L', Blocks.LADDER, 'W', new ItemStack(Blocks.WOOL, 1, color.getMetadata())));
+            GameRegistry.addRecipe(new ShapedOreRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.IRON, color), "SLS", "LWL", "SLS", 'S', Items.IRON_INGOT, 'L', Blocks.RAIL, 'W', new ItemStack(Blocks.WOOL, 1, color.getMetadata())));
+            GameRegistry.addRecipe(new ShapedOreRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.DIAMOND, color), "SLS", "LWL", "SLS", 'S', Items.DIAMOND, 'L', Blocks.RAIL, 'W', new ItemStack(Blocks.WOOL, 1, color.getMetadata())));
+            GameRegistry.addRecipe(new ShapedOreRecipe(createPipeStack(new ItemStack(RegistryEntries.ITEM.pipe, 8), EnumPipeMaterial.GOLD, color), "SLS", "LWL", "SLS", 'S', Items.GOLD_NUGGET, 'L', Blocks.GOLDEN_RAIL, 'W', new ItemStack(Blocks.WOOL, 1, color.getMetadata())));
+        }
+        GameRegistry.addRecipe(new CoverPipeRecipe());
+        RecipeSorter.register("jp.crafterkina.pipes:pipe_cover", CoverPipeRecipe.class, RecipeSorter.Category.SHAPED, "after:minecraft:shaped before:minecraft:shapeless");
+    }
+
+    private ItemStack createPipeStack(ItemStack stack, EnumPipeMaterial material, EnumDyeColor color){
+        return createPipeStack(stack, material, true, ItemDye.DYE_COLORS[color.getDyeDamage()]);
+    }
+
+    private ItemStack createPipeStack(ItemStack stack, EnumPipeMaterial material){
+        return createPipeStack(stack, material, false, -1);
+    }
+
+    private ItemStack createPipeStack(ItemStack stack, EnumPipeMaterial material, boolean covered, int color){
+        NBTTagCompound compound = new NBTTagCompound();
+        {
+            stack.setTagCompound(compound);
+            compound.setInteger("material", material.ordinal());
+            compound.setBoolean("covered", covered);
+            compound.setInteger("color", color);
+        }
+        return stack;
     }
 }
