0cb60dae75a8e01eae839ca39b9c5f17b1e99ac7
Kina <CrafterKina@users.noreply.github.com>
2017-07-09 13:00:00 +0900

    Remove unnecessary handlers

--- src/main/java/jp/crafterkina/pipes/plugin/jei/PluginJEI.java
+++ src/main/java/jp/crafterkina/pipes/plugin/jei/PluginJEI.java
@@ -1,32 +1,24 @@
 package jp.crafterkina.pipes.plugin.jei;
 
 import jp.crafterkina.pipes.common.RegistryEntries;
-import jp.crafterkina.pipes.plugin.jei.crafting.CoverPipeWithCarpetRecipeHandler;
-import jp.crafterkina.pipes.plugin.jei.crafting.CoverPipeWithCarpetRecipeWrapper;
-import jp.crafterkina.pipes.plugin.jei.crafting.CoverPipeWithWoolRecipeHandler;
-import jp.crafterkina.pipes.plugin.jei.crafting.CoverPipeWithWoolRecipeWrapper;
-import mezz.jei.api.BlankModPlugin;
+import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
+
+import javax.annotation.Nonnull;
 
 /**
  * Created by Kina on 2016/12/25.
  */
 @JEIPlugin
-public class PluginJEI extends BlankModPlugin{
+public class PluginJEI implements IModPlugin{
     @Override
-    public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry){
+    public void registerItemSubtypes(@Nonnull ISubtypeRegistry subtypeRegistry){
         subtypeRegistry.useNbtForSubtypes(RegistryEntries.ITEM.pipe);
     }
 
     @Override
-    public void register(IModRegistry registry){
-        registry.addRecipeHandlers(
-                new CoverPipeWithCarpetRecipeHandler(),
-                new CoverPipeWithWoolRecipeHandler()
-        );
-        registry.addRecipes(CoverPipeWithCarpetRecipeWrapper.getRecipes());
-        registry.addRecipes(CoverPipeWithWoolRecipeWrapper.getRecipes());
+    public void register(@Nonnull IModRegistry registry){
     }
 }
