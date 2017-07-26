5c472d66dca275713c520ead06aa8de0de8c5397
Kina <CrafterKina@users.noreply.github.com>
2017-07-27 05:26:27 +0900

    Add Accelerator Description

--- src/main/java/jp/crafterkina/pipes/plugin/jei/PluginJEI.java
+++ src/main/java/jp/crafterkina/pipes/plugin/jei/PluginJEI.java
@@ -1,10 +1,13 @@
 package jp.crafterkina.pipes.plugin.jei;
 
 import jp.crafterkina.pipes.common.RegistryEntries;
+import jp.crafterkina.pipes.common.creativetab.EnumCreativeTab;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 
 import javax.annotation.Nonnull;
 
@@ -24,5 +27,8 @@ public class PluginJEI implements IModPlugin{
 
     @Override
     public void register(@Nonnull IModRegistry registry){
+        NonNullList<ItemStack> accelerators = NonNullList.create();
+        RegistryEntries.ITEM.strategy_acceleration.getSubItems(EnumCreativeTab.PROCESSOR.tab, accelerators);
+        registry.addIngredientInfo(accelerators, ItemStack.class, "desc.jp.crafterkina.pipes.accelerator");
     }
 }
